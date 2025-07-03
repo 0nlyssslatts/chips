f163650eb767f8e866cf7b76e481ad172d1d4d35
0nlyssslatts <sid36205@mail.ru>
2025-07-03 12:46:05 +0300

    Поправил стили

--- src/components/ChipsWrapper/ChipsWrapper.tsx
+++ src/components/ChipsWrapper/ChipsWrapper.tsx
@@ -6,7 +6,7 @@ interface ChipsWrapperProps {
     className?: string;
 }
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
 export const ChipsWrapper = ({ className }: ChipsWrapperProps) => {
     return (
